6282a9a86b00ff06c238eed849c436719032e3c3
Jeremy Alva Prathama <jeremyalvax@gmail.com>
2023-04-08 22:35:35 +0700

    added serializers for Task

--- todoish/models.py
+++ todoish/models.py
@@ -12,4 +12,7 @@ class Task(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    class Meta: 
+        ordering = ['created_at']
 
---
+++ todoish/serializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from authentication.models import Profile
+from .models import Task
+
+# class TaskSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=True, max_length=250)
+#     description = serializers.CharField(required=False, allow_blank=True, max_length=2000)
+#     is_finished = serializers.BooleanField(required=True)
+#     created_at = serializers.DateTimeField(autonowadd?)
+#     updated_at = serializers.DateTimeField()
+#     # author =
+#     category = serializers.CharField(max_length=50)
+
+#     def create(self, validated_data):
+#         """
+#         Create and return a new Task instance, based on validated_data
+#         """
+#         return Task.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+#         """
+#         Update and return an existing Task instance, based on validated_data
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.is_finished = validated_data.get('is_finished', instance.is_finished)
+#         instance.created_at = validated_data.get('created_at', instance.created_at)
+#         instance.updated_at = validated_data.get('updated_at', instance.updated_at)
+#         # instance.author
+#         instance.category = validated_data.get('category', instance.category)
+
+#         instance.save()
+#         return instance
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User()
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class TaskSerializer(serializers.ModelSerializer):
+    author = ProfileSerializer(required=True)
+    class Meta:
+        model = Task
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+
+
+
+
+
